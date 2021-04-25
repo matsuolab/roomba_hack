9342a76224c2b92e64a48f61ee5fbcdbc79b8170
arima <arijun0307@gmail.com>
2021-04-25 13:34:11 +0900

    update docker for developing PC

--- BUILD-DOCKER-IMAGE.sh
+++ BUILD-DOCKER-IMAGE.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-docker build . -f docker/Dockerfile.jetson -t roomba_hack
+IMAGE_NAME=roomba_hack
+TAG_NAME=latest
+DOCKERFILE_NAME=Dockerfile
+
+if [ "$(uname -m)" == "aarch64" ]; then
+    DOCKERFILE_NAME=Dockerfile.jetson
+    TAG_NAME=jetson
+fi
+
+docker build . -f docker/${DOCKERFILE_NAME} -t ${IMAGE_NAME}:${TAG_NAME}
--- RUN-DOCKER-CONTAINER.sh
+++ RUN-DOCKER-CONTAINER.sh
@@ -5,13 +5,31 @@ CONTAINER_NAME="roomba_hack"
 echo "$0: IMAGE=${IMAGE_NAME}"
 echo "$0: CONTAINER=${CONTAINER_NAME}"
 
-docker run -it --rm \
-    --privileged \
-    --env ROS_MASTER_URI=${ros_master_uri} \
-    --env ROS_IP=${ros_ip} \
-    --net host \
-    --volume ${PWD}/catkin_ws/:/root/roomba_hack/catkin_ws/ \
-    --volume /dev/:/dev/ \
-    --name ${CONTAINER_NAME} \
-    ${IMAGE_NAME} \
-    bash
+if [ "$(uname -m)" == "aarch64" ]; then
+    docker run -it --rm \
+        --privileged \
+        --runtime nvidia \
+        --env ROS_MASTER_URI=${ros_master_uri} \
+        --env ROS_IP=${ros_ip} \
+        --net host \
+        --volume ${PWD}/catkin_ws/:/root/roomba_hack/catkin_ws/ \
+        --volume /dev/:/dev/ \
+        --name ${CONTAINER_NAME} \
+        ${IMAGE_NAME}:jetson \
+        bash
+else
+    xhost +
+    docker run -it --rm \
+        --privileged \
+        --gpus all \
+        --env DISPLAY=${DISPLAY} \
+        --env ROS_MASTER_URI=${ros_master_uri} \
+        --env ROS_IP=${ros_ip} \
+        --net host \
+        --volume ${PWD}/catkin_ws/:/root/roomba_hack/catkin_ws/ \
+        --volume /dev/:/dev/ \
+        --volume /tmp/.X11-unix:/tmp/.X11-unix \
+        --name ${CONTAINER_NAME} \
+        ${IMAGE_NAME} \
+        bash
+fi
