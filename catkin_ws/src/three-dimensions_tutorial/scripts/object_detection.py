99def19b68213d62732640ea15ea29fb9749894a
hellososhi <22724971+hellososhi@users.noreply.github.com>
2023-07-23 16:52:31 +0900

    modify objectt_detection.py

--- catkin_ws/src/three-dimensions_tutorial/scripts/object_detection.py
+++ catkin_ws/src/three-dimensions_tutorial/scripts/object_detection.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
-from pytorchyolo import detect, models
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import cv2
 import copy
+from typing import List
+
+import cv2
+import rospy
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from ultralytics import YOLO
+from ultralytics.engine.results import Results
+
 
 class ObjectDetection:
     def __init__(self):
@@ -17,44 +19,35 @@ class ObjectDetection:
         self.detection_result_pub = rospy.Publisher('/detection_result', Image, queue_size=10)
 
         # Subscriber
-        rgb_sub = rospy.Subscriber('/camera/color/image_raw', Image, self.callback_rgb)
+        rospy.Subscriber('/camera/color/image_raw', Image, self.callback_rgb)
 
         self.bridge = CvBridge()
         self.rgb_image = None
 
+        self.model = YOLO('yolov8n.pt')
+
     def callback_rgb(self, data):
         cv_array = self.bridge.imgmsg_to_cv2(data, 'bgr8')
-        cv_array = cv2.cvtColor(cv_array, cv2.COLOR_BGR2RGB)
         self.rgb_image = cv_array
 
     def process(self):
-        path = "/root/roomba_hack/catkin_ws/src/three-dimensions_tutorial/yolov3/"
-
-        # load category
-        with open(path+"data/coco.names") as f:
-            category = f.read().splitlines()
-
-        # prepare model
-        model = models.load_model(path+"config/yolov3.cfg", path+"weights/yolov3.weights")
-
         while not rospy.is_shutdown():
             if self.rgb_image is None:
                 continue
 
-            # inference
-            tmp_image = copy.copy(self.rgb_image)
-            boxes = detect.detect_image(model, tmp_image)
-            # [[x1, y1, x2, y2, confidence, class]]
+            results: List[Results] = self.model.predict(self.rgb_image)
 
-            # plot bouding box
-            for box in boxes:
-                x1, y1, x2, y2 = map(int, box[:4])
-                cls_pred = int(box[5])
+            # plot bounding box
+            tmp_image = copy.deepcopy(self.rgb_image)
+            for result in results:
+                boxes = result.boxes.cpu().numpy()
+                names = result.names
+                x1, y1, x2, y2 = map(int, boxes.xyxy[0][:4])
+                cls_pred = boxes.cls[0]
                 tmp_image = cv2.rectangle(tmp_image, (x1, y1), (x2, y2), (0, 255, 0), 3)
-                tmp_image = cv2.putText(tmp_image, category[cls_pred], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
+                tmp_image = cv2.putText(tmp_image, names[cls_pred], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
 
             # publish image
-            tmp_image = cv2.cvtColor(tmp_image, cv2.COLOR_RGB2BGR)
             detection_result = self.bridge.cv2_to_imgmsg(tmp_image, "bgr8")
             self.detection_result_pub.publish(detection_result)
 
